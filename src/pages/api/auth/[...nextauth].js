1b63650c35f7b63810eeee35fa0dd13371f41b6e
Hasan Hafizur Rahman <hasanshanto922@gmail.com>
2023-04-09 16:33:55 +0600

    google provider

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,8 +5,11 @@ export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
+      // clientId: process.env.GOOGLE_ID,
+      // clientSecret: process.env.GOOGLE_SECRET,
+      clientId:
+        "621305768285-8r22ci9kd7qlrii5ctf7k48qkhc3perd.apps.googleusercontent.com",
+      clientSecret: "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",
     }),
     // ...add more providers here
   ],
